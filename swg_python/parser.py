352824da2c43e174bd2a10e62b61a6cc0647c557
Furkan Üzümcü <furkanuzumcu@gmail.com>
2018-01-25 21:48:36 -0500

    Make Redoc the default renderer

--- swg_python/parser.py
+++ swg_python/parser.py
@@ -7,25 +7,30 @@ if sys.version_info[0] < 3:
     import codecs
 
 
-swg_python_version = "1.0.4"
+swg_python_version = "1.0.5"
 
 
 class SwgParser:
     """
-    SwgParser is a simple parser that extracts the Swagger API documentation throughout the given folders. It works with both `Python2.7` and `Python 3.x`.
-    The Swagger documentation must be written in YAML format. The specification is the same as the Swagger specification except a few details.
+    SwgParser is a simple parser that extracts the Swagger API documentation throughout the given folders. It works
+    with both `Python2.7` and `Python 3.x`. The Swagger documentation must be written in YAML format. The specification
+    is the same as the Swagger specification except a few details.
 
     **Exceptions**
-    - A path definition **MUST** have the keys `method` and `path`. `method` key contains the standard HTTP methods and `path` is the path of the operation
-    - A definition **MUST** have the `definition` key. A block that has the `definition` key will be treated as a definition. Obviously, the value for the key
-      is the definition name.
+    - A path definition **MUST** have the keys `method` and `path`. `method` key contains the standard HTTP methods and
+      `path` is the path of the operation
+    - A definition **MUST** have the `definition` key. A block that has the `definition` key will be treated as a
+      definition. Obviously, the value for the key is the definition name.
 
     Also, you need to wrap the YAML string between `@swg_begin` and `@swg_end`.
-    The documentation can be scattered throughout the project, `SwgParser` will walk thought the files and produce a single `swagger.json` or `swagger.yaml`
-    file. For an example on how to use it, check the `example` folder. Beware, although it has valid Swagger documentation the project itself does nothing and
-    doesn't work. `SwgParser` does not depend on any framework specific properties, so it can be used with any kind of project you want.
+    The documentation can be scattered throughout the project, `SwgParser` will walk thought the files and produce a
+    single `swagger.json` or `swagger.yaml`
+    file. For an example on how to use it, check the `example` folder. Beware, although it has valid Swagger
+    documentation the project itself does nothing and doesn't work. `SwgParser` does not depend on any framework
+    specific properties, so it can be used with any kind of project you want.
 
-    `SwgParser` depends only on `PyYAML` package. And you can install it with the following command: `pip install pyyaml`
+    `SwgParser` depends only on `PyYAML` package. And you can install it with the following
+    command: `pip install pyyaml`
 
     **Example Usage**
 
@@ -38,7 +43,12 @@ class SwgParser:
 
     # Swagger Preview Support
 
-    With the release of 1.0.4 `swg-python` supports preview of the Swagger Specification using the official Swagger Editor 3.0.1 release.
+    With the release of 1.0.4 `swg-python` supports preview of the Swagger Specification using the official Swagger
+    Editor 3.0.1 release.
+
+    # v1.0.5
+
+    Redoc is now used as the default Open API renderer.
 
     """
 
@@ -105,6 +115,7 @@ class SwgParser:
             dir_path = os.path.dirname(real_path)
             js_content = "var SwaggerSpec = %s;" % (dump)
             self.write_file("%s/static/swg_python/specification.js" % (dir_path), js_content)
+            self.write_file("%s/static/swg_python/specification.json" % (dir_path), dump)
 
     def compile_folder(self, directory):
         """
--- swg_python/views.py
+++ swg_python/views.py
@@ -1,6 +1,10 @@
 # Django
 from django.shortcuts import render
-
+from django.conf import settings
 
 def render_swagger_view(request):
+    redoc_enabled = getattr(settings, 'SWG_ENABLE_REDOC', True)
+    if redoc_enabled:
+        return render(request, 'swg_python/redoc.html', {})
+
     return render(request, 'swg_python/index.html', {})
